25fe3367054de3e5ac20c68a4df3fe1f2d4f30e2
1cookspe <1cookspe@gws33870.ihdsb.ca>
2016-04-12 13:27:07 -0400

    Finished ghost, started object assignment

---
+++ src/edu/hdsb/gwss/cook/u3/NationalHockeyLeague.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.cook.u3;
+
+/**
+ *
+ * @author 1cookspe
+ */
+public class NationalHockeyLeague {
+    // Class variables
+    int numberOf;
+    
+    public NationalHockeyLeague() {
+    }
+    
+}
---
+++ src/edu/hdsb/gwss/cook/u3/Teams.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.cook.u3;
+
+/**
+ *
+ * @author 1cookspe
+ */
+public class Teams {
+    // Class variables
+    private String city;
+    private String teamName;
+    private int ranking;
+    private String captain;
+    private boolean playoffs;
+    private double goalsForAV;
+    private double goalsAgainstAV;
+    
+    public Teams() {
+    }
+    
+    
+}
