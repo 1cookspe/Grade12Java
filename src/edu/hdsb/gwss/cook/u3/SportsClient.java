523a01d8c91bea8a5b5208a16aa1d65d51c28407
1cookspe <1cookspe@hdsb.ca>
2016-04-19 09:53:23 -0400

    Update SportsClient.java

--- src/edu/hdsb/gwss/cook/u3/SportsClient.java
+++ src/edu/hdsb/gwss/cook/u3/SportsClient.java
@@ -17,42 +17,33 @@ public class SportsClient {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//        Team blueJays = new Team("Toronto", "Blue Jays");
-//        Player joshDonaldson = new Player("Josh", "Donaldsondsjfklasdjflkasdfjalsdfas");
-//        joshDonaldson.setId(4323);
-//        joshDonaldson.setAge(230.4);
-//        joshDonaldson.setNationality("American");
-//        joshDonaldson.setTeam(blueJays);
-//        joshDonaldson.setPosition("Outfield");
-//        
-//        Player jose = new Player("Jose", "Baustisa");
-//        Player sidney = new Player("Sidney", "Crosby");
-//        
-//        blueJays.add(joshDonaldson);
-//        blueJays.add(jose);
-//        blueJays.remove(jose);
-//        blueJays.remove(sidney);
-//        System.out.println(blueJays.toString());
-//        System.out.println(joshDonaldson.toString());
         
         System.out.println( "-------------------" );
         System.out.println( "Test #1 - Empty Constructor" );
         Team penguins = new Team();
+        System.out.println("Pre-condition");
         assert(penguins.getCity() == null);
         assert(penguins.getId() == 1);
+        System.out.println("Post-condition");
+        penguins.setCity("Pittsburgh");
+        penguins.setName("Penguins");
+        assert(penguins.getCity() == "Pittsburgh");
+        assert(penguins.getName() == "Penguins");
         
         System.out.println( "-------------------" );
         System.out.println( "Test #2 - isValid()" );
+        System.out.println("Pre-condition");
         assert(penguins.isValid() == false);
-        
-        penguins.setCity("Pittsburgh");
-        penguins.setName("Penguins");
         penguins.setId(314725);
+        System.out.println("Post-condition");
+        assert(penguins.isValid());
         
         System.out.println( "-------------------" );
         System.out.println( "Test #3 - add(..) invalid Player" );
         Player sidneyCrosby = new Player("Sidney", "Crosby");
+        System.out.println("PRE-CONDITION:");
         assert(penguins.getPlayers().size() == 0);
+        System.out.println("POST-CONDITION:");
         penguins.add(sidneyCrosby);
         assert(penguins.getPlayers().size() == 1);
         penguins.remove(sidneyCrosby);
@@ -61,9 +52,11 @@ public class SportsClient {
         System.out.println( "-------------------" );
         System.out.println( "Test #3 - isValid(..) invalid Team" );
         Team blueJays = new Team("Toronto", "Blue Jays");
+        System.out.println("PRE-CONDITION:");
         assert(!blueJays.isValid());
         blueJays.setId(235762);
         blueJays.setSport("baseball");
+        System.out.println("POST-CONDITION:");
         assert(blueJays.isValid());
         
         System.out.println( "-------------------" );
